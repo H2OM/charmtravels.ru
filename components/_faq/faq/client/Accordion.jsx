e1dac42dc87b5d7802586625ac4388f5e7f04b23
Dmitrii <dima.zabolotnov.02@mail.ru>
2024-07-21 17:08:04 +0300

    end faq

--- components/_faq/faq/client/Accordion.jsx
+++ components/_faq/faq/client/Accordion.jsx
@@ -1,9 +1,19 @@
-export default function Accordion({children}) {
+'use client';
+import {useRef, useState} from "react";
+
+export default function Accordion({content, transition, children}) {
+    const [isHide, setHide] = useState(false);
+    const ref = useRef(false);
 
     return (
-        <div className={"faq__accordion static__content"}>
+        <div className={"static__content faq__accordion " + (isHide ? '_active' : '')}
+             style={{transition: transition}}
+             onClick={() => setHide((prev) => !prev)}>
             {children}
+            <div className="faq__accordion__content"
+                 style={isHide ? {height: ref.current.clientHeight + 'px'} : {}}>
+                <p ref={ref} dangerouslySetInnerHTML={{__html: content}}></p>
+            </div>
         </div>
     )
-
 }
--- components/_faq/faq/server/Faq.jsx
+++ components/_faq/faq/server/Faq.jsx
@@ -1,8 +1,11 @@
 import '../faq.scss';
 import ErrorBlock from "@/lib/basecomponents/errorblock/ErrorBlock";
 import Accordion from "@/components/_faq/faq/client/Accordion";
+import ContentVisible from "@/lib/basecomponents/contentVisible/contentVisible";
 
 export default function Faq({data}) {
+    const firstColumn = data.slice(0, Math.ceil(data.length / 2));
+    const secondColumn = data.slice(Math.ceil(data.length / 2));
 
     return (
         <section className={"faq"}>
@@ -10,22 +13,40 @@ export default function Faq({data}) {
                 <h2 className={"static__title"}>Часто задаваемые вопросы</h2>
                 {
                     data &&
-                    <div className={"faq__content"}>
-                        {
-                            data.map((each, i) => {
-                                return (
-                                    <Accordion key={each.title + i}>
-                                        <div className="faq__accordion__title">
-                                            {each.title}
-                                        </div>
-                                        <div className="faq__accordion__content">
-                                            <p dangerouslySetInnerHTML={{__html: each.content}}></p>
-                                        </div>
-                                    </Accordion>
-                                )
-                            })
-                        }
-                    </div>
+                    <ContentVisible>
+                        <div className={"faq__content"}>
+                            <div className="faq__content__column">
+                                {
+                                    firstColumn.map((each, i) => {
+                                        return (
+                                            <Accordion content={each.content}
+                                                       key={each.title + i}
+                                                       transition={`0.4s ${0.2 * i}s all`}>
+                                                <div className="faq__accordion__title">
+                                                    {each.title}
+                                                </div>
+                                            </Accordion>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="faq__content__column">
+                                {
+                                    secondColumn.map((each, i) => {
+                                        return (
+                                            <Accordion content={each.content}
+                                                       key={each.title + i}
+                                                       transition={`0.4s ${(firstColumn.length * 0.2) + (0.2 * i)}s all`}>
+                                                <div className="faq__accordion__title">
+                                                    {each.title}
+                                                </div>
+                                            </Accordion>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </ContentVisible>
                 }
                 {
                     !data && <ErrorBlock/>
