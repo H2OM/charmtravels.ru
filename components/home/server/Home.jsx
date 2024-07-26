5346149f3ea2ae68f5b99cf4a0ac1a5fa6f477c2
Dmitrii <dima.zabolotnov.02@mail.ru>
2024-07-26 21:11:36 +0300

    add border

--- components/home/server/Home.jsx
+++ components/home/server/Home.jsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
 
 export default function Home() {
     return (
         <section className={"home"}>
             <div className={"container"}>
                 <div className="home__banner">
-
+                    <Image
+                        src={"/services/hotels/movenpick.jpg"}
+                        className={"hotels__content__image"}
+                        alt={"image"}
+                        height={500}
+                        width={800}
+                        quality={100}
+                        priority={true}
+                    />
                 </div>
             </div>
         </section>
